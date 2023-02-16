f0159d3ca70a85ee27e7e3bbcc60f4f2b679c2d4
ChenfeiP <peng0368@morris.umn.edu>
2023-02-16 14:50:05 -0600

    Generate todo-profile files
    Co-authored-by: Ryan Sajulga <sajul004@morris.umn.edu>

---
+++ client/src/app/todos/todo-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-profile',
+  templateUrl: './todo-profile.component.html',
+  styleUrls: ['./todo-profile.component.scss']
+})
+export class TodoProfileComponent {
+
+}
