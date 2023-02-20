1ee41dd0acd41924634c20ccee95a05ae7d01889
ChenfeiP <peng0368@morris.umn.edu>
2023-02-20 17:20:37 -0600

    Update paths for app-routing module
    Co-authored-by: Ryan Sajulga <sajul004@morris.umn.edu>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { AddUserComponent } from './users/add-user.component';
+import { TodoListComponent } from './todos/todo-list.component';
+import { TodoProfileComponent } from './todos/todo-profile.component';
+import { AddTodoComponent } from './todos/add-todo.component';
 
 // Note that the 'users/new' route needs to come before 'users/:id'.
 // If 'users/:id' came first, it would accidentally catch requests to
@@ -12,7 +15,10 @@ const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
   {path: 'users', component: UserListComponent, title: 'Users'},
   {path: 'users/new', component: AddUserComponent, title: 'Add User'},
-  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'}
+  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'},
+  {path: 'todos', component: TodoListComponent, title: 'Todos'},
+  {path: 'todos/new', component: AddTodoComponent, title: 'Add Todo'},
+  {path: 'todos/:id', component: TodoProfileComponent, title: 'Todo Profile'}
 ];
 
 @NgModule({
