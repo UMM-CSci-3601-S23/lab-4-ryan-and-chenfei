bd498e36a38a82116fdf5ab4793e35f8ab8f5b80
ChenfeiP <peng0368@morris.umn.edu>
2023-02-20 17:29:35 -0600

    Edit todo-list files based on Lab 3
    Co-authored-by: Ryan Sajulga <sajul004@morris.umn.edu>

--- client/src/app/todos/todo-list.component.spec.ts
+++ client/src/app/todos/todo-list.component.spec.ts
@@ -1,23 +1,155 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MockTodoService } from '../../testing/todo.service.mock';
+import { Todo } from './todo';
+import { TodoCardComponent } from './todo-card.component';
 import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-describe('TodoListComponent', () => {
-  let component: TodoListComponent;
+const COMMON_IMPORTS: any[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatIconModule,
+  MatSnackBarModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
+
+describe('Todo list', () => {
+
+  let todoList: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(TodoListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS],
+      declarations: [TodoListComponent, TodoCardComponent],
+      // providers:    [ TodoService ]  // NO! Don't provide the real service!
+      // Provide a test-double instead
+      providers: [{ provide: TodoService, useValue: new MockTodoService() }]
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // This constructs the `todoList` (declared
+  // above) that will be used throughout the tests.
+  beforeEach(waitForAsync(() => {
+  // Compile all the components in the test bed
+  // so that everything's ready to go.
+    TestBed.compileComponents().then(() => {
+      /* Create a fixture of the TodoListComponent. That
+       * allows us to get an instance of the component
+       * (todoList, below) that we can control in
+       * the tests.
+       */
+      fixture = TestBed.createComponent(TodoListComponent);
+      todoList = fixture.componentInstance;
+      /* Tells Angular to sync the data bindings between
+       * the model and the DOM. This ensures, e.g., that the
+       * `todoList` component actually requests the list
+       * of todos from the `MockTodoService` so that it's
+       * up to date before we start running tests on it.
+       */
+      fixture.detectChanges();
+    });
+  }));
+
+  it('contains all the todos', () => {
+    expect(todoList.serverFilteredTodos.length).toBe(3);
+  });
+
+  it('contains a todo owner \'Chenfei\'', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.owner === 'Chenfei')).toBe(true);
+  });
+
+  it('contain a todo owner \'Ryan\'', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.owner === 'Ryan')).toBe(true);
+  });
+
+  it('doesn\'t contain a todo named \'Santa\'', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.owner === 'Santa')).toBe(false);
+  });
+
+  it('has two todos that are 37 years old', () => {
+    expect(todoList.serverFilteredTodos.filter((todo: Todo) => todo.status === false).length).toBe(2);
+  });
+});
+
+/*
+ * This test is a little odd, but illustrates how we can use stubs
+ * to create mock objects (a service in this case) that be used for
+ * testing. Here we set up the mock TodoService (todoServiceStub) so that
+ * _always_ fails (throws an exception) when you request a set of todos.
+ */
+describe('Misbehaving Todo List', () => {
+  let todoList: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
+
+  let todoServiceStub: {
+    getTodos: () => Observable<Todo[]>;
+    getTodosFiltered: () => Observable<Todo[]>;
+  };
+
+  beforeEach(() => {
+    // stub TodoService for test purposes
+    todoServiceStub = {
+      getTodos: () => new Observable(observer => {
+        observer.error('getTodos() Observer generates an error');
+      }),
+      getTodosFiltered: () => new Observable(observer => {
+        observer.error('getTodosFiltered() Observer generates an error');
+      })
+    };
+
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS],
+      declarations: [TodoListComponent],
+      // providers:    [ TodoService ]  // NO! Don't provide the real service!
+      // Provide a test-double instead
+      providers: [{ provide: TodoService, useValue: todoServiceStub }]
+    });
+  });
+
+  // Construct the `todoList` used for the testing in the `it` statement
+  // below.
+  beforeEach(waitForAsync(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TodoListComponent);
+      todoList = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('generates an error if we don\'t set up a TodoListService', () => {
+    // Since calling either getTodos() or getTodosFiltered() return
+    // Observables that then throw exceptions, we don't expect the component
+    // to be able to get a list of todos, and serverFilteredTodos should
+    // be undefined.
+    expect(todoList.serverFilteredTodos).toBeUndefined();
   });
 });
--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -27,4 +27,66 @@ export class TodoListComponent implements OnInit, OnDestroy {
 
   }
 
+  /**
+   * Get the todos from the server, filtered by the role and age specified
+   * in the GUI.
+   */
+  getTodosFromServer(): void {
+    // A todo-list-component is paying attention to todoService.getTodos
+    // (which is an Observable<Todos[]>)
+    // (for more on Observable, see: https://reactivex.io/documentation/observable.html)
+    // and we are specifically watching for role and age whenever the Todo[] gets updated
+    this.todoService.getTodos({
+      category: this.todoCategory,
+      status: this.todoStatus
+    }).pipe(
+      takeUntil(this.ngUnsubscribe)
+    ).subscribe({
+      // Next time we see a change in the Observable<Todo[]>,
+      // refer to that Todo[] as returnedTodos here and do the steps in the {}
+      next: (returnedTodos) => {
+        // First, update the array of serverFilteredTodos to be the Todo[] in the observable
+        this.serverFilteredTodos = returnedTodos;
+        // Then update the filters for our client-side filtering as described in this method
+        this.updateFilter();
+      },
+      // If we observe an error in that Observable, put it in the console so we can learn more
+      error: (e) => {
+        this.snackBar.open('Problem contacting the server – try again',
+          'OK',
+          // The message will disappear after 3 seconds.
+          { duration: 3000 });
+        console.error('We couldn\'t get the list of todos; the server might be down');
+      },
+      // Once the observable has completed successfully
+      // complete: () => console.log('Todos were filtered on the server')
+    });
+  }
+
+  /**
+   * Called when the filtering information is changed in the GUI so we can
+   * get an updated list of `filteredTodos`.
+   */
+  public updateFilter(): void {
+    this.filteredTodos = this.todoService.filterTodos(
+      this.serverFilteredTodos, { owner: this.todoOwner, body: this.todoBody });
+  }
+
+  /**
+   * Starts an asynchronous operation to update the todos list
+   *
+   */
+  ngOnInit(): void {
+    this.getTodosFromServer();
+  }
+
+  /**
+   * When this component is destroyed, we should unsubscribe to any
+   * outstanding requests.
+   */
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
 }
