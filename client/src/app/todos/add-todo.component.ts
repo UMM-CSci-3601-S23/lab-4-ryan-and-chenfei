58f3ea57f579fbecb884251454a44e17294bc9e2
Chenfei Peng <peng0368@morris.umn.edu>
2023-05-08 21:17:32 -0500

    Fix add-todo functionality to work properly

--- client/src/app/todos/add-todo.component.ts
+++ client/src/app/todos/add-todo.component.ts
@@ -10,55 +10,44 @@ import { TodoService } from './todo.service';
   templateUrl: './add-todo.component.html',
   styleUrls: ['./add-todo.component.scss']
 })
-export class AddTodoComponent implements OnInit {
+export class AddTodoComponent implements OnInit{
 
   addTodoForm: UntypedFormGroup;
 
   todo: Todo;
 
-    // not sure if this name is magical and making it be found or if I'm missing something,
-  // but this is where the red text that shows up (when there is invalid input) comes from
   addTodoValidationMessages = {
     owner: [
       {type: 'required', message: 'Owner is required'},
-      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'minlength', message: 'Owner must be at least a character long'},
       {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
-      {type: 'existingOwner', message: 'Owner has already been taken'}
-    ],
-
-    category: [
-      { type: 'required', message: 'Category is required' },
-      { type: 'pattern', message: 'Category must be Software Design, Homework, Video Games, or Groceries' },
-    ],
-
-    body: [
-      {type: 'required', message: 'Body is required'},
-      {type: 'minlength', message: 'Body must be at least 2 characters long'},
-      {type: 'maxlength', message: 'Body cannot be more than 50 characters long'},
-      {type: 'existingBody', message: 'Body has already been taken'}
+      {type: 'existingName', message: 'Owner already exists'},
     ],
 
     status: [
-      { type: 'required', message: 'Status is required' },
-      { type: 'pattern', message: 'Status must be Complete or Imcomplete' },
+      {type: 'required', message: 'Must have a status'}
     ],
+
+    body: [
+      {type: 'required', message: 'A body is required'},
+      {type: 'minlength', message: 'A body must be at least a character long'},
+      {type: 'maxlength', message: 'A body cannot be more than 150 characters long'},
+    ],
+
+    category: [
+      {type: 'required', message: 'A category is required'},
+      {type: 'pattern', message: 'Category must be homework, groceries, software design, or video games'},
+    ]
   };
 
   constructor(private fb: UntypedFormBuilder, private todoService: TodoService, private snackBar: MatSnackBar, private router: Router) {
   }
 
   createForms() {
-
-    // add todo form validations
     this.addTodoForm = this.fb.group({
-      // We allow alphanumeric input and limit the length for name.
-      name: new UntypedFormControl('', Validators.compose([
+      owner: new UntypedFormControl('', Validators.compose([
         Validators.required,
-        Validators.minLength(2),
-        // In the real world you'd want to be very careful about having
-        // an upper limit like this because people can sometimes have
-        // very long names. This demonstrates that it's possible, though,
-        // to have maximum length limits.
+        Validators.minLength(1),
         Validators.maxLength(50),
         (fc) => {
           if (fc.value.toLowerCase() === 'abc123' || fc.value.toLowerCase() === '123abc') {
@@ -66,49 +55,34 @@ export class AddTodoComponent implements OnInit {
           } else {
             return null;
           }
-        },
+        }
       ])),
 
-      // Since this is for a company, we need workers to be old enough to work, and probably not older than 200.
-      age: new UntypedFormControl('', Validators.compose([
+      status: new UntypedFormControl('', Validators.compose([
+      ])),
+
+      body: new UntypedFormControl('', Validators.compose([
         Validators.required,
-        Validators.min(15),
-        Validators.max(200),
-        // In the HTML, we set type="number" on this field. That guarantees that the value of this field is numeric,
-        // but not that it's a whole number. (The todo could still type -27.3232, for example.) So, we also need
-        // to include this pattern.
-        Validators.pattern('^[0-9]+$')
+        Validators.minLength(1),
+        Validators.maxLength(150),
       ])),
 
-      // We don't care much about what is in the company field, so we just add it here as part of the form
-      // without any particular validation.
-      company: new UntypedFormControl(),
-
-      // We don't need a special validator just for our app here, but there is a default one for email.
-      // We will require the email, though.
-      email: new UntypedFormControl('', Validators.compose([
+      category: new UntypedFormControl('', Validators.compose([
         Validators.required,
-        Validators.email,
-      ])),
-
-      role: new UntypedFormControl('viewer', Validators.compose([
-        Validators.required,
-        Validators.pattern('^(admin|editor|viewer)$'),
-      ])),
+        Validators.pattern('^(homework|groceries|software design|video games)$'),
+      ]))
     });
-
   }
 
-  ngOnInit() {
-    this.createForms();
+  ngOnInit(): void {
+      this.createForms();
   }
-
 
   submitForm() {
     this.todoService.addTodo(this.addTodoForm.value).subscribe({
       next: (newID) => {
         this.snackBar.open(
-          `Added todo ${this.addTodoForm.value.name}`,
+          `Added todo for ${this.addTodoForm.value.owner}`,
           null,
           { duration: 2000 }
         );
@@ -116,13 +90,12 @@ export class AddTodoComponent implements OnInit {
       },
       error: err => {
         this.snackBar.open(
-          'Failed to add the todo',
+          'Failed to add the Todo',
           'OK',
           { duration: 5000 }
         );
       },
-      // complete: () => console.log('Add todo completes!')
     });
   }
-
 }
+
