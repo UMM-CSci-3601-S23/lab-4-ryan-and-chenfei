5fbabab96100141542aa6bea3610900e8c406158
Chenfei Peng <peng0368@morris.umn.edu>
2023-05-09 18:32:24 -0500

    Add tests for the app

--- client/cypress/e2e/app.cy.ts
+++ client/cypress/e2e/app.cy.ts
@@ -9,7 +9,7 @@ describe('App', () => {
     page.getAppTitle().should('contain', 'CSci 3601 Lab 4');
   });
 
-  it('The sidenav should open, navigate to "Users" and back to "Home"', () => {
+  it('The sidenav should open, navigate to "Users", "Todos" and back to "Home"', () => {
     // Before clicking on the button, the sidenav should be hidden
     page.getSidenav()
       .should('be.hidden');
@@ -19,6 +19,12 @@ describe('App', () => {
     page.getSidenavButton().click();
     page.getNavLink('Users').click();
     cy.url().should('match', /\/users$/);
+    page.getSidenav()
+      .should('be.hidden');
+
+    page.getSidenavButton().click();
+    page.getNavLink('Todos').click();
+    cy.url().should('match', /\/todos$/);
     page.getSidenav()
       .should('be.hidden');
 
